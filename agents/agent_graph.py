b111f0b12480b96ab4a3dc4fcde9c287bf409b2b
zzrzhou <zzrzhou@tencent.com>
2025-06-22 10:03:29 +0800

    增route node

--- agents/agent_graph.py
+++ agents/agent_graph.py
@@ -1,21 +1,41 @@
+from typing import TypedDict, List, Any
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
 from .chinese_agent import get_chinese_agent
+from .gossip_agent import get_gossip_agent
+class AgentState(TypedDict):
+    messages: List[Any]
+    # 学科
+    subject: str
+
+def decide_route(state: AgentState):
+    return "chinese"
 
 # Define the function that calls the model
-def call_chinese_teacher(state: MessagesState):
+def call_chinese_teacher(state: AgentState):
+    print('call_chinese_teacher', state)
     chinese_agent = get_chinese_agent()
     response = chinese_agent.process_query(state["messages"])
+    return {"messages": response}
+
+def call_gossip_agent(state: AgentState):
+    gossip_agent = get_gossip_agent()
+    response = gossip_agent.process_query(state["messages"])
     return {"messages": response}
 
 
 def create_workflow():
     # Define a new graph
-    graph = StateGraph(state_schema=MessagesState)
+    graph = StateGraph(state_schema=AgentState)
 
     # Define the (single) node in the graph
     graph.add_edge(START, "chinese")
+    graph.add_node("route", decide_route)
     graph.add_node("chinese", call_chinese_teacher)
+    graph.add_conditional_edges(
+        "route",
+        decide_route,
+    )
 
     # Add memory
     memory = MemorySaver()
---
+++ agents/gossip_agent.py
@@ -0,0 +1,16 @@
+"""
+闲聊Agent - 专门负责闲聊
+"""
+
+from .base_agent import BaseAgent
+
+class GossipAgent(BaseAgent):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def process_query(self, query: str) -> str:
+        return self.llm.invoke(query)
+
+
+def get_gossip_agent():
+    return GossipAgent()
